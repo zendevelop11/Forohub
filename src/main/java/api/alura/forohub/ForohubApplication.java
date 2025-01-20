392dee8fec634c03cb738ffde8cf8720d53678b1
zendevelop11 <zendevelop11@gmail.com>
2025-01-19 21:37:49 -0500

    initial commit

---
+++ src/main/java/api/alura/forohub/ForohubApplication.java
@@ -0,0 +1,13 @@
+package api.alura.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubApplication.class, args);
+	}
+
+}
